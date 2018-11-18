2599016c222bb86cfd60a2746eac6b4bc0e01b97
Kearnan Kelly <kelly@kellycode.com>
2018-11-18 08:48:03 -0600

    THREE update and can now use any size heightmap

--- README.md
+++ README.md
@@ -8,3 +8,5 @@ Just an experiment: I wanted to create a landscape-mesh after reading a heightma
 There's a blogpost on this [here](http://lukaszielinski.de/blog/posts/2014/11/07/webgl-creating-a-landscape-mesh-with-three-dot-js-using-a-png-heightmap/ "Blogpost three js landscape").
 
 (By the way, OrbitControls.js is taken from the three.js-examples. Please refer to [mrdoob/three.js ](https://github.com/mrdoob/three.js/ "Three.js repo").)
+
+11/18/2018 Updated to Three.JS r98
--- js/app.js
+++ js/app.js
@@ -1,97 +1,118 @@
+'use strict';
+
+if (!THREE) {
+    const THREE = {};
+    alert('THREE is not loaded');
+}
+
+const g_scene = new THREE.Scene();
+const g_camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
+const g_renderer = new THREE.WebGLRenderer();
+const g_controls = new THREE.OrbitControls(g_camera, g_renderer.domElement);
+
+// must be greater than 0
+const TERRAIN_HEIGHT_MOD = 0.75;
+
+g_renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(g_renderer.domElement);
+
+function init() {
+    setupCamera();
+    addLights();
+    addGround();
+    render();
+}
+
 function addLights() {
- var ambientLight = new THREE.AmbientLight(0x444444);
- ambientLight.intensity = 0.0;
- scene.add(ambientLight);
+    let ambientLight = new THREE.AmbientLight(0x444444);
+    ambientLight.intensity = 0.0;
+    g_scene.add(ambientLight);
 
- var directionalLight = new THREE.DirectionalLight(0xffffff);
-
- directionalLight.position.set(900, 400, 0000).normalize();
- scene.add(directionalLight);
+    let directionalLight = new THREE.DirectionalLight(0xffffff);
+    directionalLight.position.set(900, 400, 0).normalize();
+    g_scene.add(directionalLight);
 }
 
 function setupCamera() {
-  camera.position.z = 1000;
-  camera.position.y = 240;
-  camera.position.x = 0;
-  camera.lookAt(new THREE.Vector3(0,0,0));
+    g_camera.position.setX(1000);
+    g_camera.position.setY(240);
+    g_camera.position.setZ(0);
+    g_camera.lookAt(new THREE.Vector3(0, 0, 0));
 }
 
 //To get the pixels, draw the image onto a canvas. From the canvas get the Pixel (R,G,B,A)
 function getTerrainPixelData()
 {
-  var img = document.getElementById("landscape-image");
-  var canvas = document.getElementById("canvas");
-  
-  canvas.width = img.width;
-  canvas.height = img.height;
-  canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
+    let img = document.getElementById("landscape-image");
+    let canvas = document.getElementById("canvas");
 
-  var data = canvas.getContext('2d').getImageData(0,0, img.height, img.width).data;
-  var normPixels = []
+    if (img.width !== img.height) {
+        alert('Terrain hightmap requires equal width and heights!\nCurrent width x height is ' + img.width + " x " + img.height);
+        console.error('Terrain hightmap requires equal width and heights!\nCurrent width x height is ' + img.width + " x " + img.height)
+    }
 
-  for (var i = 0, n = data.length; i < n; i += 4) {
-    // get the average value of R, G and B.
-    normPixels.push((data[i] + data[i+1] + data[i+2]) / 3);
-  }
+    canvas.width = img.width;
+    canvas.height = img.height;
+    canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
 
-  return normPixels;
+    let data = canvas.getContext('2d').getImageData(0, 0, img.height, img.width).data;
+    let normPixels = []
+
+    for (let i = 0, n = data.length; i < n; i += 4) {
+        // get the average value of R, G and B.
+        normPixels.push((data[i] + data[i + 1] + data[i + 2]) / 3);
+    }
+
+    let terrain = {
+        data: normPixels,
+        width: img.width
+    };
+
+    return terrain;
 }
 
 function addGround() {
-  var numSegments = 100;
+    let terrain = getTerrainPixelData();
+    
+    // always 1 less than image width and image width always equals height
+    let numSegments = terrain.width - 1;
+    
+    // keep in mind, that the plane has more vertices than segments. If there's one segment, there's two vertices, if
+    // there's 10 segments, there's 11 vertices, and so forth. 
+    // The simplest is, if like here you have 100 segments, the image to have 101 pixels. You don't have to worry about
+    // "skewing the landscape" then..
 
-  var geometry = new THREE.PlaneGeometry(2400, 2400, numSegments, numSegments);
-  var material = new THREE.MeshLambertMaterial({
-    color: 0xccccff,
-    wireframe: false
-  });
+    // to check uncomment the next line, numbers should be equal
+    // console.log("length: " + terrain.length + ", vertices length: " + geometry.vertices.length);
 
-  terrain = getTerrainPixelData();  
+    let geometry = new THREE.PlaneGeometry(2400, 2400, numSegments, numSegments);
+    let material = new THREE.MeshLambertMaterial({
+        color: 0xccccff,
+        wireframe: false
+    });
 
-  // keep in mind, that the plane has more vertices than segments. If there's one segment, there's two vertices, if
-  // there's 10 segments, there's 11 vertices, and so forth. 
-  // The simplest is, if like here you have 100 segments, the image to have 101 pixels. You don't have to worry about
-  // "skewing the landscape" then..
+    for (let i = 0, l = geometry.vertices.length; i < l; i++)
+    {
+        geometry.vertices[i].z += terrain.data[i] * TERRAIN_HEIGHT_MOD;
+    }
 
-  // to check uncomment the next line, numbers should be equal
-  // console.log("length: " + terrain.length + ", vertices length: " + geometry.vertices.length);
+    geometry.computeFaceNormals();
+    geometry.computeVertexNormals();
 
-  for (var i = 0, l = geometry.vertices.length; i < l; i++)
-  {
-    var terrainValue = terrain[i] / 255;
-    geometry.vertices[i].z = geometry.vertices[i].z + terrainValue * 200 ;
-  }
+    let plane = new THREE.Mesh(geometry, material);
 
-  geometry.computeFaceNormals();
-  geometry.computeVertexNormals();
+    plane.position.set(0, 0, 0);
+    
+    // rotate the plane so up is where y is growing..
+    plane.rotation.set(-Math.PI / 2, 0, 0);
 
-  var plane = new THREE.Mesh(geometry, material);
-
-  plane.position = new THREE.Vector3(0,0,0);
-  // rotate the plane so up is where y is growing..
-
-  var q = new THREE.Quaternion();
-  q.setFromAxisAngle( new THREE.Vector3(-1,0,0), 90 * Math.PI / 180 );
-  plane.quaternion.multiplyQuaternions( q, plane.quaternion );
-
-  scene.add(plane)
+    g_scene.add(plane)
 }
 
 function render() {
-  requestAnimationFrame(render);
-  renderer.render(scene, camera);
+    requestAnimationFrame(render);
+    g_renderer.render(g_scene, g_camera);
 }
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
-var renderer = new THREE.WebGLRenderer();
-var controls = new THREE.OrbitControls( camera, renderer.domElement );
-
-setupCamera();
-addLights();
-addGround();
-
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-render();
+init();
